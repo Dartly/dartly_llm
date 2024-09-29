b32fe95b9dff44c21962e7033e61cdc0eca2bdfa
Dartly <yuequanaaaa@outlook.com>
2024-09-29 13:01:59 +0000

    修改字段校验

--- lib/app/schemas/bases_model.dart
+++ lib/app/schemas/bases_model.dart
@@ -9,34 +9,26 @@ abstract class BasesModel {
   Map<String, List<Validate>> getValidation();
 
   List<String> validate(BasesModel obj) {
-    List<String> errors = [];
-    var rules = obj.getValidation();
+  List<String> errors = [];
+  var rules = obj.getValidation();
 
-    for (var entry in rules.entries) {
-      var fieldValue = obj.toJson()[entry.key];
+  for (var entry in rules.entries) {
+    var fieldValue = obj.toJson()[entry.key];
 
-      // 如果字段值为空，提前处理 NotNull 规则
-      if (fieldValue == null || fieldValue.toString().isEmpty) {
-        for (var rule in entry.value) {
-          if (rule is NotNull && !rule.isValid(fieldValue)) {
-            errors.add(rule.message);
-          }
-        }
-        continue; // 跳过后续验证
-      }
-
-      for (var rule in entry.value) {
-        if (!rule.isValid(fieldValue)) {
-          errors.add(rule.message);
-        }
+    // 处理每个校验规则
+    for (var rule in entry.value) {
+      if (!rule.isValid(fieldValue)) {
+        errors.add(rule.message);
       }
     }
-
-    // 抛出异常并合并错误信息
-    if (errors.isNotEmpty) {
-      throw ServeException(message: errors.first);
-    }
-
-    return errors; // 返回错误信息列表（如果有的话）
   }
+
+  // 抛出异常并合并错误信息
+  if (errors.isNotEmpty) {
+    throw ServeException(message: errors.join(', '));
+  }
+
+  return errors; // 返回错误信息列表（如果有的话）
+}
+
 }
