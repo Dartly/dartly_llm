e620e906ad1366554ccc4b7cdd453c0963d29d24
Dartly <yuequanaaaa@outlook.com>
2024-09-29 14:02:05 +0000

    修改校验器

--- lib/app/http/controllers/auth_controller.dart
+++ lib/app/http/controllers/auth_controller.dart
@@ -4,13 +4,13 @@ import 'package:vania/vania.dart';
 class AuthController extends Controller {
   Future<Response> login(Request req) async {
     req.validate({
-      'login.email': 'required|email',
-      'login.password': 'required|length_between:8,12',
+      'email': 'required|email',
+      'password': 'required|length_between:8,12',
     }, {
-      'login.email.required': '邮箱不能为空',
-      'login.email.email': '邮箱格式错误',
-      'login.password.required': '密码不能为空',
-      'login.password.length_between': '密码长度必须为8-12位',
+      'email.required': '邮箱不能为空',
+      'email.email': '邮箱格式错误',
+      'password.required': '密码不能为空',
+      'password.length_between': '密码长度必须为8-12位',
     });
 
     // final post = Login.fromJson(req.toJson());
