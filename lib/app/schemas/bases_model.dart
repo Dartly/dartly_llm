b6029760067b824fdcfd606c1a3b5653b76d5fc9
Dartly <yuequanaaaa@outlook.com>
2024-09-29 12:54:00 +0000

    修改字段校验

--- lib/app/schemas/bases_model.dart
+++ lib/app/schemas/bases_model.dart
@@ -10,11 +10,21 @@ abstract class BasesModel {
 
   List<String> validate(BasesModel obj) {
     List<String> errors = [];
-
     var rules = obj.getValidation();
 
     for (var entry in rules.entries) {
       var fieldValue = obj.toJson()[entry.key];
+
+      // 如果字段值为空，提前处理 NotNull 规则
+      if (fieldValue == null || fieldValue.toString().isEmpty) {
+        for (var rule in entry.value) {
+          if (rule is NotNull && !rule.isValid(fieldValue)) {
+            errors.add(rule.message);
+          }
+        }
+        continue; // 跳过后续验证
+      }
+
       for (var rule in entry.value) {
         if (!rule.isValid(fieldValue)) {
           errors.add(rule.message);
@@ -22,10 +32,11 @@ abstract class BasesModel {
       }
     }
 
+    // 抛出异常并合并错误信息
     if (errors.isNotEmpty) {
-      throw ServeException(message: errors.first);
+      throw ServeException(message: errors.join(', '));
     }
 
-    return errors;
+    return errors; // 返回错误信息列表（如果有的话）
   }
 }
